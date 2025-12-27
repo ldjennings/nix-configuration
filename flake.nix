5dcc350b479cd2d4e133fec07b0fff5715da38c4
ldjennings <ldjennings@github.com>
2025-12-26 21:02:49 -0800

    updated flake inputs, removed nvf

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    nvf.url = "github:notashelf/nvf";
+    # nvf.url = "github:notashelf/nvf";
     stylix.url = "github:danth/stylix/release-25.05";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
