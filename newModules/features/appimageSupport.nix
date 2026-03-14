b85413fe5aa27fccfbd2931676f61d2ebdfd421f
ldjennings <ldjennings@github.com>
2026-03-14 16:50:33 -0700

    added app image support module

---
+++ newModules/features/appimageSupport.nix
@@ -0,0 +1,27 @@
+# Enables running AppImage binaries directly as executables by registering
+# a binfmt_misc handler that automatically routes execution through
+# appimage-run based on the AppImage type 2 magic bytes.
+#
+# Usage:
+#   Add self.nixosModules.appimage to your host's modules list:
+#
+#   modules = [
+#     self.nixosModules.appimage
+#   ];
+#
+#   Then AppImages can be run directly without invoking appimage-run manually:
+#
+#   chmod +x myapp.AppImage
+#   ./myapp.AppImage
+{ pkgs, ... }: {
+  flake.nixosModules.appImageSupport = {
+    boot.binfmt.registrations.appimage = {
+      wrapInterpreterInShell = false;
+      interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+      recognitionType = "magic";
+      offset = 0;
+      mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+      magicOrExtension = ''\x7fELF....AI\x02'';
+    };
+  };
+}
