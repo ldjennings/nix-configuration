4f52ca13fa564daef31b949992956121faaf0ac7
ldjennings <ldjennings@github.com>
2026-03-14 04:34:28 -0700

    added suspend timeout to hypridle

--- modules/home/hyprland/hypridle.nix
+++ modules/home/hyprland/hypridle.nix
@@ -6,19 +6,24 @@
       enable = true;
       settings = {
         general = {
+          before_sleep_cmd = "loginctl lock-session";
           after_sleep_cmd = "hyprctl dispatch dpms on";
-          ignore_dbus_inhibit = false;
-          lock_cmd = "hyprlock";
+          # ignore_dbus_inhibit = false;
+          lock_cmd = "pidof hyprlock || hyprlock";
         };
         listener = [
           {
             timeout = 180;
-            on-timeout = "hyprlock";
+            on-timeout = "loginctl lock-session";
           }
           {
             timeout = 300;
             on-timeout = "hyprctl dispatch dpms off";
             on-resume = "hyprctl dispatch dpms on";
+          }
+          {
+            timeout = 600;
+            on-timeout = "systemctl suspen";
           }
         ];
       };
