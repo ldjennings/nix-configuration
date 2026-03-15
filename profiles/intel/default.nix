cd674ae3d37a62e54bb9580ac334a3c486ac8d1a
ldjennings <ldjennings@github.com>
2026-03-15 00:13:15 -0700

    removing old driver config

--- profiles/intel/default.nix
+++ profiles/intel/default.nix
@@ -2,13 +2,13 @@
 {
   imports = [
     ../../hosts/${host}
-    ../../modules/drivers
+    # ../../modules/drivers
     ../../modules/core
   ];
   # Enable GPU Drivers
-  drivers.amdgpu.enable = false;
-  drivers.nvidia.enable = false;
-  drivers.nvidia-prime.enable = false;
-  drivers.intel.enable = true;
-  vm.guest-services.enable = false;
+  # drivers.amdgpu.enable = false;
+  # drivers.nvidia.enable = false;
+  # drivers.nvidia-prime.enable = false;
+  # drivers.intel.enable = true;
+  # vm.guest-services.enable = false;
 }
